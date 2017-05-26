e90f7dc4c430c8773e036085e857fa37fc7bd853
Alex Lee <doomcat9167@gmail.com>
2017-05-26 15:22:13 -0700

    add some buttons and widgets

--- app/src/main/java/com/doomcat/twilioapp/MainActivity.java
+++ app/src/main/java/com/doomcat/twilioapp/MainActivity.java
@@ -1,13 +1,33 @@
 package com.doomcat.twilioapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+
+    @Bind(R.id.startButton)
+    Button mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if (v == mStartButton){
+//            Intent intent = new Intent(MainActivity.this, AppActivity.class);
+//            startActivity(intent);
+        }
     }
 }
